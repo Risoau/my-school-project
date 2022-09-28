20f74cb4edfa60709081149fe6bd27dd559e0b79
FarhanRadityaAlGazali <zomnifromex@gmail.com>
2022-09-28 10:56:10 +0800

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/resource/css/style.css">
+    <title>Risoau - Raditya's Website</title>
+</head>
+<body>
+
+    <div class="div-title">
+        <h1 class="welcome-text">Welcome To</h1>
+        <h1 class="Risoau-text">Risoau</h1>
+    </div>
+    
+</body>
+</html>
